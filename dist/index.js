44d03e53ecef662b206d4f7c7a360452ec3ddaff
Ingyu Yoon <loveloper44@gmail.com>
2020-02-01 12:46:47 +0900

    Build index.js

--- dist/index.js
+++ dist/index.js
@@ -828,7 +828,7 @@ function render(item) {
     }
 
     if (typeof item === "string" && item.match(/^\%b/)) {
-      return process.env[item.substr(3)];
+      return process.env[item.substr(3)] === "true";
     }
 
     return item;
